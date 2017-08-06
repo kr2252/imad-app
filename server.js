6394004ce9afa55db16835a94e6dfb5f52363a7e
kr2252 <30746609+kr2252@users.noreply.github.com>
2017-08-06 20:00:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-1', function (req, res) {
+    res.send('this is good');
+});
+
+app.get('article-2', function (req, res) {
+    res.send('this is better');
+});
+
+app.get('article-3', function (req, res) {
+    res.send('this is ugly');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
